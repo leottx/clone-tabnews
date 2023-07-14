4a40e98513b26f0b64135e62b2a6e6198e4d4120
leotx <tteixeira.leonardo@gmail.com>
2023-07-14 15:35:14 -0300

    adiciona uma exclamacao

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div style={{margin: "auto", maxWidth: 400, padding: 20, textAlign: "center"}}>
-            <h1>Eu te amo, meu amor!</h1>
+            <h1>Eu te amo, meu amor!!!</h1>
             <p>O momento não é fácil, mas sei que é uma fase que será superada. Obrigado por todo o seu incondicional apoio.</p>
             <p>Você é meu mundo 💖</p>
         </div>
